7003173758d385f31964070e3e8ff73abf1765b9
akeruzunosmanoglu <75013943+akeruzunosmanoglu@users.noreply.github.com>
2021-03-18 09:59:27 +0100

    Update mychrony.pp

--- site/profile/manifests/mychrony.pp
+++ site/profile/manifests/mychrony.pp
@@ -1,8 +1,6 @@
 class profile::mychrony {   
     class { 'chrony':
-      #servers => lookup('chrony::servers')
-      servers => lookup('benim::kron')
-      #servers         => [ 'ntp1.corp.com', 'ntp22.corp.com', ],
+   
  }
    
 }
